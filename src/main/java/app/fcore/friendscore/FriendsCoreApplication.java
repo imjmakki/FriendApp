d12f5f68ba51e3106a11a3d8d6829593347de7bc
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-18 16:36:23 +0300

    Back End Code

---
+++ src/main/java/app/fcore/friendscore/FriendsCoreApplication.java
@@ -0,0 +1,13 @@
+package app.fcore.friendscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/fcore/friendscore/FriendsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package app.fcore.friendscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
