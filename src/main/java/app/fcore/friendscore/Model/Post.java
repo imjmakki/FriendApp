f4a3d89278477ef63e2cb5c55e770762fb8b4b01
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-20 15:54:57 +0300

    Back End Code

---
+++ src/main/java/app/fcore/friendscore/Model/Post.java
@@ -0,0 +1,14 @@
+package app.fcore.friendscore.Model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "posts")
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+}
